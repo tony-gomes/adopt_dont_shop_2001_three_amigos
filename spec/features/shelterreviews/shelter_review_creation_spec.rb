99916cf4b4524a432e0152dc688ef5cf27f83785
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-25 09:34:39 -0600

    remove comments

--- spec/features/shelterreviews/shelter_review_creation_spec.rb
+++ spec/features/shelterreviews/shelter_review_creation_spec.rb
@@ -31,16 +31,3 @@ RSpec.describe "when i visit a shelter show page and click new review link" do
     end
   end
 end
-
-
-# As a visitor,
-# When I visit a shelter's show page
-# I see a link to add a new review for this shelter.
-# When I click on this link, I am taken to a new review path
-# On this new page, I see a form where I must enter:
-# - title
-# - rating
-# - content
-# I also see a field where I can enter an optional image (web address)
-# When the form is submitted, I should return to that shelter's show page
-# and I can see my new review

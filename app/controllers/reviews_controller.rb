ba2dcf0e3760cedec980b382df5b4b735c575660
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-25 18:37:12 -0600

    Changes from merge issues

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,19 +13,22 @@ class ReviewsController < ApplicationController
       flash[:error] = "Review must include a title, rating, and content to be submitted."
     end
   end
+  
+  def edit
+    @review = Review.find(params[:review_id])
+  end
 
   def update
     review = Review.find(params[:review_id])
     review.update(review_params)
-    redirect_to "/shelters/#{@review.shelter_id}"
+    redirect_to "/shelters/#{review.shelter_id}"
   end
 
   def destroy
     Review.destroy(params[:review_id])
     redirect_to "/shelters/#{params[:shelter_id]}"
-  def edit
-    @review = Review.find(params[:review_id])
   end
+
 
   private
 

83deaf2c30534d381d97a8693118efdf4885d730
Mike Hernandez <hernandezmichael321@gmail.com>
2020-03-26 22:46:51 -0600

    Create test and update routes to make favorites into a link

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   patch '/shelters/:shelter_id/reviews/:review_id', to: 'reviews#update'
   delete '/shelters/:shelter_id/reviews/:review_id', to: 'reviews#destroy'
 
+  get '/favorite', to: 'favorite#index'
   patch '/favorite/:pet_id', to: 'favorite#update'
 
 end

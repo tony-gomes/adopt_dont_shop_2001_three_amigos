9f099c5bca8bfb1129a06ed1acc91b5921c83dc0
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-28 16:26:40 -0600

    add final model and test updates

---
+++ app/models/application_pet.rb
@@ -0,0 +1,6 @@
+class ApplicationPet < ApplicationRecord
+
+  belongs_to :pet
+  belongs_to :pet_application
+
+end
---
+++ spec/models/application_pet_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe ApplicationPet, type: :model do
+
+  describe "relationships" do
+    it { should belong_to :pet_application }
+    it { should belong_to :pet }
+  end
+
+end

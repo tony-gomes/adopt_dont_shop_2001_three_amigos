cb34cc1f563d64082ad92c2cade0f3575810b89d
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-29 12:14:50 -0600

    fix typo in pet app controller

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -15,7 +15,7 @@ class PetApplicationsController < ApplicationController
         pet.update(adopt_status: "Pending")
         favorite.contents.delete(pet_id.to_i)
       end
-      flash[:submit_app_success] = "Your applicion was submitted successfully!"
+      flash[:submit_app_success] = "Your application was submitted successfully!"
       redirect_to "/favorites"
     else
       redirect_back(fallback_location: root_path)
